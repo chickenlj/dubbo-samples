1b8a67572f3ed21f7c36cf97e7e9eb2d0ba31102
ken.lj <ken.lj.hz@gmail.com>
2019-09-04 10:41:23 +0800

    2.6.2 edas troubleshooting

--- dubbo-samples-annotation/src/main/java/org/apache/dubbo/samples/annotation/AnnotationProviderBootstrap.java
+++ dubbo-samples-annotation/src/main/java/org/apache/dubbo/samples/annotation/AnnotationProviderBootstrap.java
@@ -19,15 +19,12 @@
 
 package org.apache.dubbo.samples.annotation;
 
-import com.alibaba.dubbo.config.ApplicationConfig;
-import com.alibaba.dubbo.config.ProtocolConfig;
-import com.alibaba.dubbo.config.ProviderConfig;
-import com.alibaba.dubbo.config.RegistryConfig;
-import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.apache.dubbo.samples.annotation.support.EmbeddedZooKeeper;
+
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 public class AnnotationProviderBootstrap {
 
@@ -40,37 +37,9 @@ public class AnnotationProviderBootstrap {
 
     @Configuration
     @EnableDubbo(scanBasePackages = "org.apache.dubbo.samples.annotation.impl")
+    @ImportResource("classpath:/spring/dubbo.xml")
     static class ProviderConfiguration {
-        @Bean
-        public ProviderConfig providerConfig() {
-            ProviderConfig providerConfig = new ProviderConfig();
-            providerConfig.setTimeout(1000);
-            return providerConfig;
-        }
 
-        @Bean
-        public ApplicationConfig applicationConfig() {
-            ApplicationConfig applicationConfig = new ApplicationConfig();
-            applicationConfig.setName("dubbo-annotation-provider");
-            return applicationConfig;
-        }
-
-        @Bean
-        public RegistryConfig registryConfig() {
-            RegistryConfig registryConfig = new RegistryConfig();
-            registryConfig.setProtocol("zookeeper");
-            registryConfig.setAddress("localhost");
-            registryConfig.setPort(2181);
-            return registryConfig;
-        }
-
-        @Bean
-        public ProtocolConfig protocolConfig() {
-            ProtocolConfig protocolConfig = new ProtocolConfig();
-            protocolConfig.setName("dubbo");
-            protocolConfig.setPort(20880);
-            return protocolConfig;
-        }
     }
 
 }
